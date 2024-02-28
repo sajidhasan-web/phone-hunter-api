db94e04dcff87b1451203b5ea11eb692427e95da
sajidhasan-web <sajidhasan.web@gmail.com>
2024-02-28 15:10:43 +0600

    data load done

--- js/phone.js
+++ js/phone.js
@@ -1,20 +1,9 @@
-const loadPhone = async () => {
-  const res = await fetch(
-    "https://openapi.programming-hero.com/api/phones?search=iphone"
-  );
-  const data = await res.json();
-  const phones = data.data
-//   console.log(phones)
-   displayPhone(phones)
-};
+const loadPhone = async () =>{
+    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
 
-
-const displayPhone = phones =>{
- phones.forEach( phone => {
-    console.log(phone)
- });
+    const data = await res.json()
+    const phones = data.data
+    console.log(phones)
 }
 
-
-
-loadPhone()
+loadPhone();
